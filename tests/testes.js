8a87b240a5935de7298b68d7876ce3a364cd1fda
JoãoVitorrCnr <167380615+JoaoVitorrCnr@users.noreply.github.com>
2025-01-22 21:21:51 -0300

    Adiciona testes para cálculo com valores negativos

--- tests/testes.js
+++ tests/testes.js
@@ -3,3 +3,7 @@ const { calcularJurosCompostos } = require('../src/calculo');
 
 // Teste 1: Valores positivos
 assert.strictEqual(calcularJurosCompostos(1000, 5, 2), 1102.5, "Erro no cálculo para valores positivos");
+
+// Teste 2: Valores negativos
+assert.throws(() => calcularJurosCompostos(-1000, 5, 2), "Entradas inválidas");
+console.log("Todos os testes passaram.");
